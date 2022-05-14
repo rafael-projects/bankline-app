cad03d184dfff19b5b6aaaaca94ee03fedbbf645
rafael-projects <68446421+rafael-projects@users.noreply.github.com>
2022-05-14 04:06:30 -0300

    bottstrap-add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'movimentacao-list', component : MovimentacaoListComponent },
+  { path: 'movimentacao-new', component : MovimentacaoNewComponent},
+  { path: '', redirectTo : 'movimentacao', pathMatch : 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
